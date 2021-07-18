aa888df315a1ac7a1280485857b434ebc266bd37
Carlos-maldonado578 <carlos.maldonado7@gmail.com>
2021-07-17 20:25:30 -0400

    Se agrega bienvenida a user y se declaran variables del juego.

---
+++ adivina_numero_computadora.py
@@ -0,0 +1,12 @@
+def adivina_numero_computadora(x):
+
+    print("=========================")
+    print("¡Bienvenido(a) al Juego!")
+    print("=========================")
+
+    print(f"Selecciona un numero entre 1 y {x} para que la computadora intente adivinarlo... ")
+
+    limite_inferior = 1
+    limite_superior = x
+
+    repuesta = ""
